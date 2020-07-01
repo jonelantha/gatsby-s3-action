2b082bf12409311082d3ac59568b259983eb9d26
jonelantha <57597570+jonelantha@users.noreply.github.com>
2020-07-01 16:35:10 +0100

    Cloudfront tests

--- src/aws/cloudfront.ts
+++ src/aws/cloudfront.ts
@@ -4,9 +4,11 @@ export async function invalidateCloudfront({
   cloudfrontID
 }: InvalidateCloudfrontParams): Promise<void> {
   await exec(
-    `aws cloudfront create-invalidation \
-    --distribution-id ${cloudfrontID} \
-    --paths /*`
+    [
+      'aws cloudfront create-invalidation',
+      `--distribution-id ${cloudfrontID}`,
+      '--paths /*'
+    ].join(' ')
   )
 }
 
